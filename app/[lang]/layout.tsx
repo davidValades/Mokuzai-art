7aaa1d9ceb4dd887f63d9f1305e63ad785ef7f6c
david <davidvaladesnavarro@gmail.com>
2026-05-04 16:46:39 +0200

    añadiendo link a github

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter, Cormorant_Garamond } from "next/font/google";
 import "../globals.css";
@@ -134,6 +135,7 @@ export default async function RootLayout({
             </CartProvider>
           </I18nProvider>
         </AuthProvider>
+        <Analytics />
       </body>
     </html>
   );
